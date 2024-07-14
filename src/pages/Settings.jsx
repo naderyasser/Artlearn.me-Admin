1881317b07df845c18fe1dffda60cbd375ca75f4
h4x0r <naderyasser023@gmail.com>
2024-07-14 08:48:19 -0400

    first commit

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Pagination from "../components/Pagination";
+
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+const Settings = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (Cookies.get("user") === undefined) {
+      Cookies.set("user", "false");
+      Cookies.set("website", "artlearn");
+    }
+    if (Cookies.get("user") === "false") {
+      navigate("/signin");
+    }
+  }, [navigate]);
+  return (
+    <div>
+      {/* <Table /> */}
+      <Pagination />
+    </div>
+  );
+};
+
+export default Settings;
